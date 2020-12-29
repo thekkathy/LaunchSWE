ed85d0262958af9d5f23f58c979f568e382b5545
Kathy Kang <kck3due@virginia.edu>
2020-12-29 16:58:11 -0500

    deleted folder

---
+++ xmas/client/src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import history from '../history';
+
+import Header from './Header';
+import OpenPresent from './OpenPresent';
+import Load from './Load';
+import Present from './Present';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Router history={history}>
+                    <Header />
+                    <Route path="/" exact component={OpenPresent} />
+                    <Route path="/load" exact component={Load} />
+                    <Route path="/present/:type" exact component={Present} />
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default App;
